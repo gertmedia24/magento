92c1d5a5c9e83ad72f68acdac74b79ade108c284
gert <gert.vantonder@media24.com>
2017-10-10 17:04:13 +0200

    Sect 3: Writing module installation scripts

---
+++ app/code/local/MasteringMagento/Example/sql/example_setup/mysql4-upgrade-0.0.0-0.0.1.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this Mage_Core_Model_Resource_Setup */
+$this->startSetup();
+
+$this->run("
+CREATE TABLE {$this->getTable('example/event')}
+        (
+            `event_id` INT AUTO_INCREMENT PRIMARY KEY,
+            `name` VARCHAR(255),
+            `start` DATETIME,
+            `end` DATETIME,
+            `created_at` DATETIME,
+            `modified_at` DATETIME
+        );
+");
+
+$this->endSetup();
+
