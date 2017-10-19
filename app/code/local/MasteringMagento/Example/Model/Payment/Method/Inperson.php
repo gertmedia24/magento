61de909a4aafaad5f145d677ccd61c9408eee74b
gert <gert.vantonder@media24.com>
2017-10-19 16:43:02 +0200

    Sect 6: Add custom payment method (in-person payment method)

---
+++ app/code/local/MasteringMagento/Example/Model/Payment/Method/Inperson.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Sect 6: Custom payment method
+ */
+
+
+class MasteringMagento_Example_Model_Payment_Method_Inperson extends Mage_Payment_Model_Method_Abstract
+{
+    protected $_code = 'inperson';
+
+
+}
